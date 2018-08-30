fabeb6c840b77f1322e938547a276fd944fc6c57
couldbefree <propokerstar90@gmail.com>
2018-08-30 15:30:50 +0300

    Enabled routing, created new components

---
+++ src/components/Products/NewProduct.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <h1>New product</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "New product",
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
